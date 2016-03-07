5bbf01656b7a516d707c81553933e393276a37ec
KellyYang <housemeow@gmail.com>
2016-03-07 10:41:53 +0800

    add timer, play music function

--- KyTimer/KyTimer/ViewController.swift
+++ KyTimer/KyTimer/ViewController.swift
@@ -10,21 +10,39 @@ import Cocoa
 
 class ViewController: NSViewController {
     @IBOutlet weak var timerTextField: NSTextField!
+    var timer: NSTimer? = nil;
+    var minute: Double = 0;
+    var content: String = "要做事啦！";
     
-    @IBAction func clickButton(sender: AnyObject) {
-        print(timerTextField.stringValue);
-    }
-
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
-
+    
+    @IBAction func fireTimer(sender: AnyObject) {
+        minute =  self.stringToNumber(timerTextField.stringValue);
+        timer = NSTimer.scheduledTimerWithTimeInterval(minute*60, target: self, selector: "update", userInfo: nil, repeats: false)
+    }
+    
     override var representedObject: AnyObject? {
         didSet {
-        // Update the view, if already loaded.
+            // Update the view, if already loaded.
         }
+    }
+    
+    func playMusic(name: String) {
+        let sound = NSSound(named: name);
+        sound?.play();
+    }
+    
+    func stringToNumber(string: String)->Double {
+        return NSString(string: string).doubleValue;
+    }
+    
+    func update() {
+        print("Minute:" + String(minute));
+        self.playMusic("pikapi.mp3");
     }
 }
 
